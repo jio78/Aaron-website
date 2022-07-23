62648874f35157ed6933c2acaf7cdc7caa1b81ab
jio78 <86997527+jio78@users.noreply.github.com>
2022-07-22 17:29:00 -0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,9 @@
+const headerHtml = `<div class="nav-left">
+<a><h2>A.I.M. Bot Robotics</h2></a>
+</div>
+<div class="nav-right">
+<a href="./index.html"><h3>Home</h3></a>
+<a href="./robot-list.html"><h3>Robots</h3></a>
+<a href="./members.html"><h3>About Us</h3></a>
+</div>`
+$('header').html(headerHtml)
